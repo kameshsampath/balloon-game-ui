715dea482cef1349060365653dae77b206ca68ef
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-09-25 18:55:01 +0530

    make it html only project

--- src/test/java/com/redhat/developers/demo/NativeGameResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.developers.demo;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGameResourceIT extends GameResourceTest {
-
-    // Execute the same tests but in native mode.
-}
